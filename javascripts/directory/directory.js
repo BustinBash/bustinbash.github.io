3399bdeffd6c814f5a050d5e4416b17b9ed35a3e
Arik Gadye <arikdovgadye@gmail.com>
2014-08-16 16:10:38 -0700

    directory controller/view setup, ready for additional functions to render file animations

--- javascripts/directory/directory.js
+++ javascripts/directory/directory.js
@@ -1,16 +1,47 @@
 // Model
-Directory.Model = function(){
+// Directory.Model = function(){
 
-}
+// }
 
 // View
-Directory.View = function(){
+Directory.View = function(){}
 
+Directory.View.prototype = {
+	renderBeforeDirectory: function(before){
+		$("#" + before).css("background-color", "yellow");
+	},
+	renderAfterDirectory: function(before, after) {
+		$("#" + before).css("background-color", "")
+		$("#" + after).css("background-color", "yellow")
+	}
+}
+
+
+Directory.Controller = function(view){
+	this.view = view;
+}
+
+Directory.Controller.prototype = {
+	init: function() {
+		this.bindListeners();
+	},
+	bindListeners: function() {
+		$(document).on('changeLevel', function(event, data){
+			this.after = data.After;
+			this.before = data.Before;
+			this.createBeforeDir(this.before);
+		}.bind(this));
+
+		$(document).on('success', function(){
+			this.createAfterDir(this.before, this.after)
+		}.bind(this));
+	},
+	createBeforeDir: function(before){
+		this.view.renderBeforeDirectory(before);
+	},
+	createAfterDir: function(before, after) {
+		this.view.renderAfterDirectory(before, after)
+	}
 }
 
 // Controller
-Directory.Controller = function(){
-  this.init = function(){
-    console.log("Directory Controller")
-  }
-}
