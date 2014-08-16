07e33c468a5e431ab97b590c661005c0c63a1339
Arik Gadye & Puru Dahal <pair+arikgadye.dahal@devbootcamp.com>
2014-08-15 18:32:21 -0700

    Break render success and error to their respective functions

--- javascripts/terminal/terminal.js
+++ javascripts/terminal/terminal.js
@@ -1,16 +1,15 @@
 var data =
-  {
-    "Title": "Print Working Directory",
-    "Definition": "pwd (print working directory): This command will return the path of the directory you are currently viewing.",
-    "Challenge": "Please type 'pwd' below.",
-    "Answer": "pwd",
-    "Hint1": "You want to Print the Working Directory to continue.",
-    "Hint2": "Come on, we have it quoted just up there!",
-    "Error": "Seems like you didn't type 'pwd'.  Try again.",
-    "Success": "/usa/california/san_francisco"
-  }
+{
+  "Title": "Print Working Directory",
+  "Definition": "pwd (print working directory): This command will return the path of the directory you are currently viewing.",
+  "Challenge": "Please type 'pwd' below.",
+  "Answer": "pwd",
+  "Hint1": "You want to Print the Working Directory to continue.",
+  "Hint2": "Come on, we have it quoted just up there!",
+  "Error": "Seems like you didn't type 'pwd'.  Try again.",
+  "Success": "/usa/california/san_francisco"
+}
 
-//debugger
 // Model
 Terminal.Model = function(){
 
@@ -19,16 +18,30 @@ Terminal.Model = function(){
 // View
 Terminal.View = function(){
   this.appendFeed = function(input){
-      // alert('hi')
       if (input === data.Answer){
-
-        $('.feed').append("<p class='term-name'> BustinBash:- " + input +
-          "<p class='term-success'>" + data.Success + '</p>')
+        this.renderSuccess(input)
       } else {
-        $('.feed').append("<p class='term-name'> BustinBash:- " + input +
-          "<p class='term-error'>" + data.Error + '</p>')
+        this.renderError(input)
       }
+      this.clearText()
+    }
 
+    this.renderSuccess = function(input){
+      $('.feed').append(
+        "<p class='term-name'> BustinBash:- " + input +
+        "<p class='term-success'>" + data.Success + '</p>'
+        )
+    }
+
+    this.renderError = function(input){
+      $('.feed').append(
+        "<p class='term-name'> BustinBash:- " + input +
+        "<p class='term-error'>" + data.Error + '</p>'
+        )
+    }
+
+    this.clearText = function(){
+      $('input').val("")
     }
   }
 
@@ -48,7 +61,7 @@ Terminal.Controller = function(view, model){
   this.checkKey = function(e) {
     if (e.which == 13) {
       var input = $('.input input').val();
-      console.log(this.view.appendFeed(input))
+      this.view.appendFeed(input)
     }
   }
 }
