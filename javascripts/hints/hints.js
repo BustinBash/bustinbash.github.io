2a570d17f142f1c428f620e68af276cd5e768d40
Brian <bajunio@gmail.com>
2014-08-16 19:47:07 -0700

    add hint controller

--- javascripts/hints/hints.js
+++ javascripts/hints/hints.js
@@ -1,16 +1,33 @@
 // Model
-Hints.Model = function(){
-
-}
+Hints.Model = function(){}
 
 // View
-Hints.View = function(){
+Hints.View = function(){}
 
+Hints.View.prototype = {
+  render: function() {
+    console.log("hi there b")
+  }
 }
 
 // Controller
-Hints.Controller = function(){
-  this.init = function(){
-    console.log("Hints Controller")
-  }  
+Hints.Controller = function(view){
+  this.view = view;
+}
+Hints.Controller.prototype = {
+  init: function(){
+    this.bindListeners();
+  },
+  bindListeners: function() {
+    $(document).on('changeLevel', function(event, data){
+      this.data = data
+    }.bind(this));
+
+    $('#hint').on('click', function(){
+      this.displayHint(this.data.hint);
+      }.bind(this));
+  },
+  displayHint: function(hint){
+    this.view.render(hint)
+  }
 }
