350ff71cf224f8fd9c2b3e9c714bcef7e5aef9a3
Brian <bajunio@gmail.com>
2014-08-19 23:04:45 -0700

    refactor the wall of functionCollection by abstracting the elements to the JSON file

--- javascripts/directory/directory.js
+++ javascripts/directory/directory.js
@@ -1,93 +1,21 @@
 BustinBash.Directory.View = function() {}
 
 BustinBash.Directory.View.prototype = {
-	functionCollection: {
-		// ~> pwd
-		1: function() {
 
-		},
-
-		// ~> ls
-		2: function(data){
-
-		},
-
-		// ~> cd usa
-		3: function(data){
-			$('.states, #divider3').show();
+	hideAndShowDOM: function(data) {
+			$(data.Hide).hide();
+			$(data.Show).show();
+			this.clearDom();
 			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> cd ..
-		4: function(data){
-			$('.states, .places, #divider3').hide();
-			$('#divider4').show();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> cd usa/ca/san_francisco
-		5: function(data){
-			$('.states, .cities, .places, #divider3, #divider4').show();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> touch the_bay.txt
-		6: function(data){
-			$('#the_bay').show();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> rm the_bay.txt
-		7: function(data){
-			$('#the_bay').hide();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> cd ..
-		8: function(data){
-			$('.places').hide();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> mkdir marin
-		9: function(data){
-			$('#marin').show();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> rmdir marin
-		10: function(data){
-			$('#marin').hide();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> cd
-		11: function(data){
-			$('.states, .cities, .places, #divider3, #divider4').hide();
-			this.appendCurrentFolder(data.Target)
-			this.clearDom()
-		},
-
-		// ~> echo Bustin Bash
-		12: function(){
-
 		},
 
 		clearDom: function() {
 			$('#directory-template li').hide();
 		},
+
 		appendCurrentFolder: function(data) {
 			$('.current-folder').text(data)
 		}
-	}
 
 }
 
@@ -110,12 +38,8 @@ BustinBash.Directory.Controller.prototype = {
 		}.bind(this));
 	},
 
-	thisLevel: function(data){
-		this.view.functionCollection[(data.ID -1)](data);
-	},
-
 	checkLevel: function(data) {
-		this.view.functionCollection[data.ID](data);
+		this.view.hideAndShowDOM(data);
 	}
 
 }
