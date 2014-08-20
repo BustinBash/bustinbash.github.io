68afdd87765559beb43ce305882b7137cc7d0ed7
Brian <bajunio@gmail.com>
2014-08-19 19:26:10 -0700

    refactor createNavigationButtons to loop directly through the data object rather than creating an array first

--- javascripts/lessons/lessons.js
+++ javascripts/lessons/lessons.js
@@ -4,7 +4,7 @@ BustinBash.Lessons.View = function() {}
 BustinBash.Lessons.View.prototype = {
   render: function(navigationButton) {
     $('#navbar_popup').append(navigationButton)
-}
+  }
 }
 
 //----- Controller -------
@@ -16,18 +16,18 @@ BustinBash.Lessons.Controller = function(view) {
 BustinBash.Lessons.Controller.prototype = {
  init: function() {
   this.bindListeners();
- },
-
- bindListeners: function() {
-  $(document).on('getData', function(event, data) {
-      this.createNavigationButtons(this.db);
-    }.bind(this))
 },
-//  TODO:  Look into iterating directly through data rather than making an array
- createNavigationButtons: function(data) {
-  var lessonArray = Object.keys(data)
-  for (var i = 0; i < lessonArray.length; i++) {
-    this.view.render("<div class='tooltip levels' id='" + lessonArray[i] + "' data-tooltip-content='" + data[lessonArray[i]].Title + "'><img class='little' src='images/terminal.png'></div>")
+
+bindListeners: function() {
+  $(document).on('getData', function(event, data) {
+    this.createNavigationButtons(data);
+  }.bind(this))
+},
+
+createNavigationButtons: function(data) {
+  for (lesson in data) {
+    this.view.render("<div class='tooltip levels' id='" + lesson + "' data-tooltip-content='" + lesson.Title + "'><img class='little' src='images/terminal.png'></div>")
   }
 }
 }
+
